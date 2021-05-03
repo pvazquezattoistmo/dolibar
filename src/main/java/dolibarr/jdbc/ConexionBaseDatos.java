0a52bbf13c8efdffac21764007e801cdeb4111d9
pvazquezattoistmo <pvazquez.attoistmo@gmail.com>
2021-05-03 13:38:19 -0500

    cambios 2

--- src/main/java/dolibarr/jdbc/ConexionBaseDatos.java
+++ src/main/java/dolibarr/jdbc/ConexionBaseDatos.java
@@ -14,18 +14,18 @@ import java.sql.SQLException;
  * @author PC
  */
 public class ConexionBaseDatos {
-    
+
     private static final String JDBC_URL = "jdbc:mysql://localhost/dolibarr?useSSL=false&serverTimezone=America/Mexico_City&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "admin";
-    
+    private static final String JDBC_PASSWORD = "";
+
     private static Connection conn;
-    
-    public static Connection getInstance() throws SQLException{
-        if (conn == null){
+
+    public static Connection getInstance() throws SQLException {
+        if (conn == null) {
             conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
         }
         return conn;
     }
-    
+
 }
